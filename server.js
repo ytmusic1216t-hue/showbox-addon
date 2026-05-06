86931297153146511717518257f5e6cadbcbc542
ytmusic1216t-hue <ytmusic1216t@gmail.com>
2026-05-06 18:39:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require("express");
-const app = express();
+const http = require("http");
 
-app.get("/manifest.json", (req, res) => {
-  const token = req.query.token || "";
+const PORT = process.env.PORT || 3000;
 
-  res.json({
-    name: "ShowBox Private",
-    version: "2.0.0",
-    scrapers: [{
-      id: "sb-private",
-      name: "ShowBox",
-      settings: [{ name: "token_box", type: "text" }],
-      extra: { token }
-    }]
-  });
-});
+http.createServer((req, res) => {
+  if (req.url.startsWith("/manifest.json")) {
+    const url = new URL(req.url, "http://localhost");
+    const token = url.searchParams.get("token") || "";
 
-app.listen(3000);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+      name: "ShowBox Private",
+      version: "2.0.0",
+      token
+    }));
+  } else {
+    res.writeHead(200);
+    res.end("OK");
+  }
+}).listen(PORT, "0.0.0.0", () => console.log("Running"));
